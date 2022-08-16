01c7366d9b5bd4794dc54ce21b7b74949cfc50e6
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2022-08-16 23:40:56 +0530

    add road map file for project

---
+++ index.js
@@ -0,0 +1,36 @@
+Front-end ==> 
+                step 1 ==>  
+                            Navbar ==> 
+                                        Public ==>
+                                                    1 - Logo
+                                                    2 - Home
+                                                    3 - About
+                                                    4 - SignIn
+                                                    5 - SignUp
+                                        private ==>
+                                                    1 - Logo
+                                                    2 - Home
+                                                    3 - About
+                                                    4 - Profile ==> 
+                                                                    1 - LogOut
+
+
+
+
+Back-end ==> 
+                step 1 ==>  
+                            Navbar ==> 
+                                        Public ==>
+                                                    1 - Home
+                                                    2 - About
+                                                    3 - SignIn ==> 
+                                                                    1 - Google SignIn
+                                                                    2 - Email and Password
+                                                    4 - SignUp ==> 
+                                                                    1 - Google SignUp
+                                                                    2 - Fullname, Email and Password ==> 
+                                                                                                            1 - Email OTP Varification
+                                                    5 - LogOut                                                   
+
+
+
